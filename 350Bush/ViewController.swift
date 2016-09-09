18f9713f2085b2f193b22c383acbafa5ebe26f45
Aubrey Goodman <aubrey.goodman@gmail.com>
2016-09-09 11:11:09 -0700

    Initial Commit

---
+++ 350Bush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  350Bush
+//
+//  Created by Aubrey Goodman on 9/9/16.
+//  Copyright © 2016 Aubrey Goodman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
