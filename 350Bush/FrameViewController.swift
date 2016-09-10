20fa9f0d6b873828ab18b496ce69e78670baa511
Aubrey Goodman <aubrey.goodman@gmail.com>
2016-09-10 09:55:02 -0700

    initial commit

---
+++ 350Bush/FrameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FrameViewController.swift
+//  350Bush
+//
+//  Created by Aubrey Goodman on 9/9/16.
+//  Copyright © 2016 Aubrey Goodman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FrameViewController : UIViewController {
+  
+  @IBOutlet var imageView: UIImageView?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+  }
+  
+}
+
