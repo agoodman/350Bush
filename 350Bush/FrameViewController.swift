4dd17e53239258856763072091363186fd700cb7
Aubrey Goodman <aubrey.goodman@gmail.com>
2016-09-11 15:39:03 -0700

    introduce separation of axes in frame browser
    user testing shows pan operations in both time and space are confusing.
    to simplify, we isolate touch movement along an active axis.
    
    activating an axis requires the pan gesture to translate beyond a
    threshold. once active, discrete impulse events are emitted when pan
    velocity exceeds an axis-specific threshold.
    
    two sliders represent the current value of time and space. both are
    currently rendered horizontally. this is not ideal for time, which is
    interactive vertically.

--- 350Bush/FrameViewController.swift
+++ 350Bush/FrameViewController.swift
@@ -11,18 +11,30 @@ import UIKit
 
 class FrameViewController : UIViewController {
   
-  let panThreshold : Float = 4
+  let panThreshold : Float = 20
+  let spaceThreshold : Float = 10
+  let timeThreshold : Float = 50
   
   @IBOutlet var imageView: UIImageView?
+  @IBOutlet var horizontalSlider: UISlider?
+  @IBOutlet var verticalSlider: UISlider?
+  
   var i: UInt8 = 0
   var j: UInt8 = 0
   var lastUpdateTime: NSDate = NSDate.init()
+  var iActive: Bool = false
+  var jActive: Bool = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    self.horizontalSlider?.continuous = false
+    self.horizontalSlider?.hidden = true
+    self.verticalSlider?.continuous = false
+    self.verticalSlider?.hidden = true
+    
     // create pan gesture recognizer to track touches
-    let recognizer : UIPanGestureRecognizer = UIPanGestureRecognizer.init(target: self, action: Selector("handlePan:"))
+    let recognizer : UIPanGestureRecognizer = UIPanGestureRecognizer.init(target: self, action: #selector(handlePan))
     self.view.addGestureRecognizer(recognizer)
     
     ImageManager.sharedInstance.fetchManifest() {
@@ -45,61 +57,78 @@ class FrameViewController : UIViewController {
   
   @IBAction func up() {
     if( self.i < UInt8(ImageManager.sharedInstance.gridSize) - 1 ) {
+      NSLog("ImageManager.up")
       self.i += 1
+      self.loadCurrentThumb()
+      self.preloadThumbs()
     }
-    self.loadCurrentFrame()
-    self.preloadThumbs()
   }
   
   @IBAction func down() {
     if( self.i > 0 ) {
+      NSLog("ImageManager.down")
       self.i -= 1
-      self.loadCurrentFrame()
+      self.loadCurrentThumb()
       self.preloadThumbs()
     }
   }
 
   @IBAction func left() {
     if( self.j > 0 ) {
+      NSLog("ImageManager.left")
       self.j -= 1
-      self.loadCurrentFrame()
+      self.loadCurrentThumb()
       self.preloadThumbs()
     }
   }
   
   @IBAction func right() {
     if( self.j < UInt8(ImageManager.sharedInstance.gridSize) - 1 ) {
+      NSLog("ImageManager.right")
       self.j += 1
-      self.loadCurrentFrame()
+      self.loadCurrentThumb()
       self.preloadThumbs()
     }
   }
   
   private func preloadThumbs() {
   
-    let delta : UInt8 = ImageManager.sharedInstance.delta
-    let iRange : Range = createRange(self.i, delta: delta)
-    let jRange : Range = createRange(self.j, delta: delta)
-    ImageManager.sharedInstance.fetchThumbs(iRange, jRange: jRange, progress: {
+    let iDelta : UInt8 = (self.iActive ? ImageManager.sharedInstance.delta : 1)
+    let jDelta : UInt8 = (self.jActive ? ImageManager.sharedInstance.delta : 1)
+    let iRange : Range = createRange(self.i, delta: iDelta)
+    let jRange : Range = createRange(self.j, delta: jDelta)
+    ImageManager.sharedInstance.fetchRange(iRange, jRange: jRange, quality: .Thumb, progress: {
       (complete: UInt, total: UInt) in
       
-      NSLog("progress: %d of %d", complete, total)
+//      NSLog("progress: %d of %d", complete, total)
     }) {
       (success: Bool) in
       
-      let val = success ? "OK" : "FAIL"
-      NSLog("ImageManager.preloadThumbs %@", val)
-      self.loadCurrentFrame()
+      self.loadCurrentThumb()
     }
     
   }
   
-  private func loadCurrentFrame() {
-    ImageManager.sharedInstance.loadImage(self.i, j: self.j) {
+  private func loadCurrentFrame(quality: Quality) {
+    self.horizontalSlider?.setValue(Float(self.j), animated: false)
+    self.verticalSlider?.setValue(Float(self.i), animated: false)
+        
+    ImageManager.sharedInstance.loadImage(self.i, j: self.j, quality: quality) {
       (image: UIImage) in
       
       self.imageView?.image = image
     }
+  }
+
+  private func loadCurrentThumb() {
+    NSLog("Frame.loadThumb %d, %d", self.i, self.j)
+    
+    loadCurrentFrame(.Thumb)
+  }
+  
+  @objc private func loadCurrentFull() {
+    NSLog("Frame.loadFull %d, %d", self.i, self.j)
+    loadCurrentFrame(.Full)
   }
   
   private func loadManifestContents() {
@@ -115,6 +144,10 @@ class FrameViewController : UIViewController {
       NSLog("ImageManager.loadManifest OK (size: %d)", ImageManager.sharedInstance.gridSize)
       self.i = 0
       self.j = UInt8(ImageManager.sharedInstance.gridSize / 2)
+      self.horizontalSlider?.minimumValue = 0
+      self.horizontalSlider?.maximumValue = Float(ImageManager.sharedInstance.gridSize)
+      self.verticalSlider?.minimumValue = 0
+      self.verticalSlider?.maximumValue = Float(ImageManager.sharedInstance.gridSize)
       self.preloadThumbs()
     }
     
@@ -130,26 +163,49 @@ class FrameViewController : UIViewController {
   }
   
   @IBAction func handlePan(gestureRecognizer: UIPanGestureRecognizer) {
-    if( gestureRecognizer.state == .Changed ) {
+    if( gestureRecognizer.state == .Ended ) {
+      self.iActive = false
+      self.jActive = false
+      self.horizontalSlider?.hidden = true
+      self.verticalSlider?.hidden = true
+    }
+    else if( gestureRecognizer.state == .Changed ) {
+      let translation = gestureRecognizer.translationInView(self.view)
       let velocity = gestureRecognizer.velocityInView(self.view)
       let projectedDate : NSDate = self.lastUpdateTime.dateByAddingTimeInterval(0.1)
       let now : NSDate = NSDate.init()
       if( projectedDate.compare(now) == .OrderedAscending ) {
-        if( Float(velocity.x) > self.panThreshold ) {
+        if( !self.iActive && !self.jActive && gestureRecognizer.numberOfTouches() == 1 && (Float(translation.x) > self.panThreshold || Float(translation.x) < -self.panThreshold) ) {
+          self.horizontalSlider?.hidden = false
+          self.verticalSlider?.hidden = true
+          self.iActive = true
+          self.jActive = false
           self.lastUpdateTime = NSDate.init()
-          self.left()
         }
-        else if( Float(velocity.x) < -self.panThreshold ) {
+        else if( !self.iActive && !self.jActive && gestureRecognizer.numberOfTouches() == 1 && (Float(translation.y) > self.panThreshold || Float(translation.y) < -self.panThreshold) ) {
+          self.horizontalSlider?.hidden = true
+          self.verticalSlider?.hidden = false
+          self.iActive = false
+          self.jActive = true
           self.lastUpdateTime = NSDate.init()
-          self.right()
         }
-        else if( Float(velocity.y) > self.panThreshold ) {
+        else if( self.iActive ) {
+          if( Float(velocity.x) > self.spaceThreshold ) {
+            left()
+          }
+          else if( Float(velocity.x) < -self.spaceThreshold ) {
+            right()
+          }
           self.lastUpdateTime = NSDate.init()
-          self.down()
         }
-        else if( Float(velocity.y) < -self.panThreshold ) {
+        else if( self.jActive ) {
+          if( Float(velocity.y) > self.timeThreshold ) {
+            down()
+          }
+          else if( Float(velocity.y) < -self.timeThreshold ) {
+            up()
+          }
           self.lastUpdateTime = NSDate.init()
-          self.up()
         }
       }
     }
